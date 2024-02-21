0c8782bcd150d605a9f360a19eb8b6aa8742d08b
kenzie.evan <kenzie.evan@sunterra.id>
2024-02-21 01:27:42 -0500

    First Iteration of Note Adding, Editing, and Deleting

--- app/src/main/java/com/example/shopping_companion/MainActivity.kt
+++ app/src/main/java/com/example/shopping_companion/MainActivity.kt
@@ -1,11 +1,141 @@
 package com.example.shopping_companion
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.AdapterView.OnItemLongClickListener
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+
+/*
+Note Taking Project
+Authors: Kenzie Evan and Kenneth Nguyen
+Mobile Application Security CSCI 445
+
+*/
 
 class MainActivity : AppCompatActivity() {
+
+    // Note Variables
+//    var notes = ArrayList<String>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ListView of notes
+        var notesList = findViewById<ListView>(R.id.notesList)
+
+
+
+        // Using Shared Preferences for local storage of notes
+        val sharedPreferences = applicationContext.getSharedPreferences("NotesApplication", MODE_PRIVATE)
+        val set = sharedPreferences.getStringSet("notes", null) as HashSet<String>?
+
+        if(set == null)
+        {
+            notes.add("Example Note")
+        }
+        else
+        {
+            notes = ArrayList(set)
+        }
+
+
+        // Initialize ListView Adapter
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, notes)
+        notesList.adapter = arrayAdapter
+
+
+        // Method to move to Note Editor Activity by clicking on ListView
+        notesList.onItemClickListener = OnItemClickListener { parent, view, position, id ->
+            val intent = Intent(
+                applicationContext,
+                NoteEditingActivity::class.java
+            )
+
+            intent.putExtra("noteID", position) // to tell us which row of listView was tapped
+
+            startActivity(intent)
+        }
+
+        // Code to Enable Deletion. Prompt by long clicking
+        notesList.onItemLongClickListener = OnItemLongClickListener { parent, view, position, id ->
+            AlertDialog.Builder(this@MainActivity)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Delete?")
+                .setMessage("Are you sure you want to delete this note?")
+                .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, which ->
+                    notes.removeAt(position)
+                    arrayAdapter!!.notifyDataSetChanged()
+
+                    // Updates the local storage after deleting
+                    val sharedPreferences = applicationContext.getSharedPreferences("NotesApplication", MODE_PRIVATE)
+                    val set = HashSet(notes)
+                    sharedPreferences.edit().putStringSet("notes", set).apply()
+                })
+                .setNegativeButton("No", null)
+                .show()
+            true
+        }
+
+        // Create New Note Button Implementation
+        var addNoteButton = findViewById<Button>(R.id.addNoteButton)
+
+        addNoteButton.setOnClickListener { //
+            val intent = Intent(
+                applicationContext,
+                NoteEditingActivity::class.java
+            )
+            startActivity(intent)
+        }
+
+        arrayAdapter!!.notifyDataSetChanged()
     }
+
+    override fun onResume() {
+        super.onResume()
+        // Make sure the array adapter edits. Possibly could raise issue if main activity is not resumed
+        arrayAdapter!!.notifyDataSetChanged()
+    }
+
+
+    /*
+    Option Menu Handling Section
+    Give ability to create notes from the menu
+    */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.note_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        if (item.itemId == R.id.add_note) {
+            val intent = Intent(
+                applicationContext,
+                NoteEditingActivity::class.java
+            )
+            startActivity(intent)
+            return true
+        }
+        return false
+    }
+
+    // Static Variables that can be used anywhere
+    companion object {
+        var arrayAdapter: ArrayAdapter<String>? = null
+        var notes = ArrayList<String>()
+    }
+
+
 }
---
+++ app/src/main/java/com/example/shopping_companion/NoteEditingActivity.kt
@@ -0,0 +1,66 @@
+package com.example.shopping_companion
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.example.shopping_companion.MainActivity.Companion.arrayAdapter
+import com.example.shopping_companion.MainActivity.Companion.notes
+
+
+class NoteEditingActivity : AppCompatActivity() {
+
+    // Var to keep track of current page
+    var noteID = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_editing)
+
+        // Initialize note page
+        val editText = findViewById<View>(R.id.notePage) as EditText
+        val intent = intent
+
+        // Variables from Main Activity
+        noteID = intent.getIntExtra("noteID", -1) // So if empty it goes to new Note
+
+        // Get content of notes in String ArrayList
+        if(noteID != -1)
+        {
+            if (notes != null) {
+                editText.setText(notes[noteID])
+            };
+        }
+        else
+        {
+            if (notes != null) {
+                // Process of Creating a new Note if the ID does not exist
+                notes.add("")
+                noteID = notes.size -1
+            }
+        }
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (notes != null) {
+                    notes[noteID] = s.toString()
+                    arrayAdapter!!.notifyDataSetChanged()
+
+                    // Stores the notes we have changed
+                    val sharedPreferences = applicationContext.getSharedPreferences("NotesApplication", MODE_PRIVATE)
+                    val set = HashSet(notes)
+                    sharedPreferences.edit().putStringSet("notes", set).apply()
+
+                }
+
+            }
+
+            override fun afterTextChanged(s: Editable) {}
+        })
+    }
+}
